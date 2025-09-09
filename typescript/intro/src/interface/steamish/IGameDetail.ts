e6781be091896b9e96f4bead65857f4a9e66062d
Nicolas P <nicolas.puissant.pro@gmail.com>
2025-09-09 14:27:50 +0200

    exo steamish fin

---
+++ typescript/intro/src/interface/steamish/IGameDetail.ts
@@ -0,0 +1,12 @@
+interface IGameDetail
+{
+    id: number,
+    name: string,
+    rating: number,
+    released: string,
+    background_image: string,
+    slug: string,
+    description: string,
+}
+
+export default IGameDetail;
--- typescript/intro/src/steamish.ts
+++ typescript/intro/src/steamish.ts
@@ -1,8 +1,10 @@
 import IGame from "./interface/steamish/IGame";
+import IGameDetail from "./interface/steamish/IGameDetail";
 import IResponse from "./interface/steamish/IResponse";
 
 let page: number = 1;
 let isFetching:boolean = false;
+let key:string = '8663bd76098941f2852fcd0bbb1d700e';
 
 let gameListDiv: HTMLDivElement | null = document.querySelector("#gameList");
 let searchBarElement: HTMLInputElement | null = document.querySelector("#searchBar");
@@ -14,18 +16,16 @@ function findNextGames():void
     if (gameListDiv != null) 
     {
         isFetching = true;
-        let apiCall:string = 'https://api.rawg.io/api/games?key=' + '8663bd76098941f2852fcd0bbb1d700e'+ '&page=' + page;
+        let apiCall:string = 'https://api.rawg.io/api/games?key=' + key + '&page=' + page;
         if (searchBarElement != null && searchBarElement.value != "") 
         {
             apiCall += "&search=" + searchBarElement.value;    
         }
         fetch(apiCall)
         .then((response: Response): Promise<any> => {
-            console.log(response);
             return response.json()
         })
         .then((data: IResponse):void => {
-            console.log(data);
             let games: Array<IGame> = data.results;
 
             
@@ -33,8 +33,10 @@ function findNextGames():void
             {
                 let gameElement: HTMLDivElement = document.createElement("div");
                 gameElement.innerHTML = 
+                "<a href='/steamish/game.html?id=" + game.id + "'>" +
                 "<h2>" + game.name + "</h2>" +
-                "<img src='" + game.background_image + "' height='100px'>";
+                "<img src='" + game.background_image + "' height='100px'>" +
+                "</a>";
                 gameListDiv.append(gameElement);
             }
             page++;
@@ -68,5 +70,38 @@ if (searchBarElement != null)
         }, 500);
 
     })    
+}
+
+const gameDiv: HTMLDivElement | null = document.querySelector("#gameDiv");
+if (gameDiv != null) 
+{
+    let params = new URLSearchParams(document.location.search);
+    let gameId: string | null = params.get("id");
+
+    fetch('https://api.rawg.io/api/games/' + gameId + '?key=' + key)
+    .then((response: Response): Promise<any> => {
+        return response.json()
+    })
+    .then((game: IGameDetail):void => 
+    {
+        const title:HTMLTitleElement | null = document.querySelector('title');
+        if (title != null) 
+        {
+            title.textContent = game.name;
+
+            const gameDiv: HTMLDivElement | null = document.querySelector("#gameDiv");
+
+            if (gameDiv != null) 
+            {
+                gameDiv.innerHTML = 
+                "<h2>" + game.name + "</h2>" +
+                "<img src='" + game.background_image + "' height='500px'>" +
+                "<p>" + game.description + "</p>" +
+                "<p>Score : " + game.rating + "</p>" +
+                "<p>Date de publication : " + game.released + "</p>"; 
+            }
+        }
+
+    }); 
 }
 
